0193f6fe6b553873904c92ecb3681152c63a8fd2
gera1013 <gera1013@users.noreply.github.com>
2019-04-13 07:52:05 -0600

    Version 1.2

--- app/src/main/java/com/example/lab4/MainActivity.kt
+++ app/src/main/java/com/example/lab4/MainActivity.kt
@@ -2,9 +2,9 @@ package com.example.lab4
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-//import android.databinding.DataBindingUtil
+import android.databinding.DataBindingUtil
 import android.support.v4.widget.DrawerLayout
-//import com.example.lab4.databinding.ActivityMainBinding
+import com.example.lab4.databinding.ActivityMainBinding
 import androidx.navigation.Navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.NavigationUI
@@ -16,11 +16,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        //val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
         val navController = findNavController(this, R.id.myNavHostFragment)
         NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout)
         appBarConfiguration = AppBarConfiguration(navController.graph, drawerLayout)
-        //NavigationUI.setupWithNavController(binding.navView, navController)
+        NavigationUI.setupWithNavController(binding.navView, navController)
     }
 
     override fun onSupportNavigateUp(): Boolean {
